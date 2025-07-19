fd6085495f1ca5cf317d15bba7d4a5d914e2f6da
Collin Matz <collin.matz.a@gmail.com>
2025-07-19 11:50:34 -0500

    only pushed chanegs from one folder. pushing others (#2)

--- run.sh
+++ run.sh
@@ -1,2 +1,25 @@
-# Script to run the entire application!
-python ./backend/app.py
+#!/bin/bash
+
+# Script to run the entire application
+
+cleanup() {
+    echo "Caught Ctrl-C - cleaning up..."
+
+    # Move back to parent directory on kill
+    cd ..
+}
+
+trap cleanup INT
+trap cleanup EXIT
+
+echo "Running webapp..."
+cd ./qgames || exit 1
+
+# Run Python in the background
+python -m server.app
+# PY_PID=$!
+
+# Wait on the Python process
+# wait $PY_PID
+
+echo "Finished."
